0b73ff121bbccf1f4ab5e93323dff021d8590cad
Rizlan Tamima <rizlan@ojekbro.com>
2022-06-30 16:36:18 +0700

    sign up

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 class MainController < ApplicationController
 
     def index
-        
+    # flash.now[:notice] = "Logged in succesfully" 
+    # flash.now[:alert] = "Wrong email or password!" 
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get "about-us", to: "about#index", as: :about
 
+  get "sign-up", to: "registration#new", as: :sign_up
+  post "sign-up", to: "registration#create", as: :sign_up_create
+
   root to: "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
